1bc2b43618665929c8e22afc5227168f644ecced
Kevin Sullivan <sullivan@virginia.edu>
2025-01-15 22:41:32 -0500

    Introduction and making sure everything's working.

--- code/DMTL4/Lectures/02_prop_logic/formal/semantics.lean
+++ code/DMTL4/Lectures/02_prop_logic/formal/semantics.lean
@@ -3,7 +3,7 @@ import DMTL4.Lectures.«02_prop_logic».formal.domain
 
 namespace DMTL4.lecture.prop_logic
 
-/-!
+/- @@@
 ### Semantics
 
 The idea of semantics in Propositional Logic is simple:
@@ -18,14 +18,14 @@ subexpressions, getting Bools, which you then combing
 using the Boolean function specified by the *syntactic*
 operator (logical connective): and, or, not, or whatever.
 That's it!
--/
+@@@ -/
 
-/-!
+/- @@@
 #### Interpretation of Unary Connectives
 
 The first thing we'll do is define what Boolean operators
 we mean by the names of our unary and binary "conenctives".
--/
+@@@ -/
 
 -- function takes unary operator and returns *unary* Boolean function
 -- (Bool -> Bool) means takes *one* Bool argument and "returns" a Bool
@@ -34,12 +34,12 @@ def evalUnOp : UnOp → (Bool → Bool)
 | (UnOp.not) => not
 
 
-/-!
+/- @@@
 #### Interpretation of Binary Connectives
 
 - takes a binary operator and returns corresponding *binary* Boolean function
 - (Bool → Bool → Bool means takes two Bools and finally returns one at the end)
--/
+@@@ -/
 
 def evalBinOp : BinOp → (Bool → Bool → Bool)
 | BinOp.and => and
@@ -47,7 +47,7 @@ def evalBinOp : BinOp → (Bool → Bool → Bool)
 | BinOp.imp => imp
 | BinOp.iff => iff
 
-/-!
+/- @@@
 We've now understood that an "interpretation" can be understood
 to be and can at least here actually be *used* as a function that
 takes a variable (var) as an argument and that returns the Boolean
@@ -57,13 +57,13 @@ To understand the next line, understand that (var → Bool) in Lean
 is the type of any function that takes a var argument and returns a
 Bool value. Here we just give this *type* a name to make subsequent
 code just a easier for people to read and understand.
--/
+@@@ -/
 
 abbrev BoolInterp := BoolVar → Bool
 
 open PLExpr
 
-/-!
+/- @@@
 #### Operational Semantics of Propositional Logic
 
 NB: This is the material you most need and want to grok.
@@ -76,7 +76,7 @@ The first line defines evalBoolExpr to be some function taking
 an expression, e, and an interpretation, i, as arguments and
 returning the Boolean meaining of e in the "world" (binding
 of all variables to Boolean values) expressed by that i.
--/
+@@@ -/
 
 def evalPLExpr : PLExpr → BoolInterp → Bool
 | lit_expr b,             _ => b
@@ -84,9 +84,10 @@ def evalPLExpr : PLExpr → BoolInterp → Bool
 | (un_op_expr op e),      i => (evalUnOp op) (evalPLExpr e i)
 | (bin_op_expr op e1 e2), i => (evalBinOp op) (evalPLExpr e1 i) (evalPLExpr e2 i)
 
-/-!
+/- @@@
 That's it. From this material you should be able to aquire
 a justifiably confident grasp of essentially every aspect
 of the syntax and semantics of propositional logic.
--/
+@@@ -/
+
 end DMTL4.lecture.prop_logic
--- code/DMTL4/Lectures/02_prop_logic/formal/syntax.lean
+++ code/DMTL4/Lectures/02_prop_logic/formal/syntax.lean
@@ -1,6 +1,6 @@
 namespace DMTL4.lecture.prop_logic
 
-/-!
+/- @@@
 # Propositional Logic: Syntax
 
 This section of the exam simply includes our formal
@@ -38,9 +38,9 @@ that Lean checks automatically for syntactic correctness.
 Let's dive down into literal, variable, and application
 (operator application, if you want) expressions and how
 we represent them in Lean.
--/
+@@@ -/
 
-/-!
+/- @@@
 ### Literal Expressions
 
 Our implementation of propositional logic defines two
@@ -49,9 +49,9 @@ lit, (b) from one of the two Boolean values, provided
 as an argument (true or false). Our syntactic language
 now includes two expressions: (lit true) and (lit false).
 In concrete syntax ⊤ is (lit true) and ⊥ is (lit false).
--/
+@@@ -/
 
-/-!
+/- @@@
 ### Variable Expressions
 
 Variable expressions are a little more complicated, but
@@ -72,21 +72,21 @@ more.
 We define *var* to be the type of *variables* as we've
 been using the term: they are not themselves expressions
 but are necessary values for making variable expressions.
--/
+@@@ -/
 
-/-!
+/- @@@
 ##### abstract syntax for variables (var)
 
 The idea is that eventually an interpretation function
 will take a BoolVar object as arguments and return the
 Boolean values that that interpretation assigns to it.
--/
+@@@ -/
 structure BoolVar : Type :=
   mk :: (index: Nat)
 deriving Repr
 
 
-/-!
+/- @@@
 The structure keyword indicates we're defining a data
 type with just one constructor, here called *mk*. In this
 case, it takes one argument, a natural number. The result
@@ -96,9 +96,9 @@ our set of infinitely many, built from the Nat value 3".
 Here's a shorthand notation we just made up. You can use
 var_[3], for example, to mean (var.mk 3). It't not a lot
 of help, but
--/
+@@@ -/
 
-/-!
+/- @@@
 ##### Concrete syntax for variables (var)
 
 We could define our own concrete notation for variables,
@@ -109,13 +109,13 @@ This constructor will take zero or more argument. So, to
 build a value of such a type you can use ⟨ a, b, c,... ⟩
 notation as a shorthand for, say, "var.mk a b c ....".
 We do need to let Lean know the result show be a "var".
--/
+@@@ -/
 #check (⟨3⟩ : BoolVar)    -- it's a variable (var)
 -- #check ⟨3⟩             -- not unique constructor expr
 -- But where Lean can infer BoolVar type ⟨3⟩ will suffice
 
 
-/-!
+/- @@@
 Now we just defined variables expressions as expressions
 of the form (var_expr v). We define var_expr shortly. View
 var_expr as the method for constructing a variable expression
@@ -124,9 +124,9 @@ get the underlying variable back out to work with. We'll
 need that when it comes to defining interpretations as
 functions that take *variables* in and that return values
 from the semantic domain, here just of Boolean values.
--/
+@@@ -/
 
-/-!
+/- @@@
 ### Operator Expressions
 
 Next we define the *expression-composing operator* (also
@@ -142,7 +142,7 @@ We call the only unary operator *not*, and the usual binary operators
 by their usual names. Remeber, these are meaningless symbols in the formal
 *syntax* of propositional logic. We give them names that will reflect their
 usual meanings when we define *semantics* for syntact expressions.
--/
+@@@ -/
 
 -- unary connective (syntactic expression composers)
 inductive UnOp : Type
@@ -157,7 +157,7 @@ inductive BinOp : Type
 | iff
 deriving Repr
 
-/-!
+/- @@@
 Now we get to the heart of the matter. With those preliminary
 dependencies out of the way, we now formally specify the complete
 (abstract) syntax of propositional logic. We do this by defining
@@ -167,7 +167,7 @@ fact, in Lean, this type specifies the set of *all* and *only*
 the expressions legal in propositional logic.
 
 ### Formal Syntax of Propositional Logic
--/
+@@@ -/
 
 inductive PLExpr : Type
 | lit_expr (from_bool : Bool) : PLExpr
@@ -176,15 +176,15 @@ inductive PLExpr : Type
 | bin_op_expr (op : BinOp) (e1 e2 : PLExpr)
 deriving Repr
 
-/-!
+/- @@@
 The "deriving Repr" here is a detail you can ignore for
 now. It's explained in the book. In a nutshell it causes
 Lean to try to synthesize a function to convert any value
 of this type to a string to help in presenting values as
 properly formatted output strings. Anyway, a detail.
--/
+@@@ -/
 
-/-
+/- @@@
 Every type encloses the names of its constructors
 in a snamespace with the same name as the type. So
 Expr is now a namespace, and the constructor names
@@ -193,7 +193,7 @@ etc. To avoid having to repeat that Expr. bit all
 the time, one can "open" the namespace. Just don't
 do this if it would result in names having multiple
 different definitions being "directly visible."
--/
+@@@ -/
 open PLExpr
 
 
