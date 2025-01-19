73b805374152009e76abe446af5ce0bf59f3add4
Kevin Sullivan <sullivan@virginia.edu>
2025-01-19 01:14:08 -0500

    .

--- code/DMT1/Library/propLogic/semantics.lean
+++ code/DMT1/Library/propLogic/semantics.lean
@@ -34,11 +34,20 @@ represent an interpretation.
 
 def Interp := (Var → Bool)
 
+/- @@@
+Semantic evaluation of an expression e under and interpretation i
+@@@ -/
+
 def eval : Expr → Interp → Bool
 | lit_expr b,             _ => b
 | (var_expr v),           i => i v
 | (un_op_expr op e),      i => (evalUnOp op) (eval e i)
 | (bin_op_expr op e1 e2), i => (evalBinOp op) (eval e1 i) (eval e2 i)
 
+/- @@@
+The standard notation for (eval e i) is
+@@@ -/
+
+notation "⟦" e "⟧" i => (eval e i)
 
 end DMT1.Library.propLogic.semantics
