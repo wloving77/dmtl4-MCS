9a2e42d364fb5644c8eb604bca810b759c3ebd83
Kevin Sullivan <sullivan@virginia.edu>
2025-01-21 23:41:21 -0500

    cleanup

--- code/DMT1.lean
+++ code/DMT1.lean
@@ -1,6 +1,8 @@
 -- This module serves as the root of the `DMT1` library.
 -- Import modules here that should be built as part of the library.
 
+-- KS: When adding imports, lake build is very helpful
+
 import DMT1.Lectures.L02_propLogic.formal.syntax
 import DMT1.Lectures.L02_propLogic.formal.semantics
 import DMT1.Lectures.L02_propLogic.formal.interpretation
@@ -9,10 +11,15 @@ import DMT1.Lectures.L02_propLogic.formal.axioms
 import DMT1.Lectures.L02_propLogic.formal.identities
 import DMT1.Lectures.L02_propLogic.formal.utilities
 
-
 import DMT1.hw.hw3.syntax
 import DMT1.hw.hw3.domain
 import DMT1.hw.hw3.semantics
 import DMT1.hw.hw3.axioms
 import DMT1.hw.hw3.interpretation
 import DMT1.hw.hw3.utilities
+
+import DMT1.Lectures.L03_modelTheory.models
+import DMT1.Lectures.L03_modelTheory.counterexamples
+import DMT1.Lectures.L03_modelTheory.truthTable
+import DMT1.Lectures.L03_modelTheory.properties
+import DMT1.Lectures.L03_modelTheory.validity
